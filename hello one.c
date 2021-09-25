2ddc7fba2a8cae3fca53706962712025594fd43e
haidang1709 <91364145+haidang1709@users.noreply.github.com>
2021-09-25 13:51:26 +0700

    Add files via upload

---
+++ hello one.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("nam trong tam tri\n");
+	printf("vay ma nguoi khong biet\n");
+	printf("cu sot sang tim kiem tu ben ngoai\n");
+	return 0;
+}
